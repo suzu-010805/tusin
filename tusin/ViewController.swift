993de367550ab62e67abaf251ccced8acf7cbce9
pipinosuke <ys.pipinosuke@gmail.com>
2020-01-18 13:49:33 +0900

    Initial Commit

---
+++ tusin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tusin
+//
+//  Created by kids on 2020/01/18.
+//  Copyright © 2020 kids. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
